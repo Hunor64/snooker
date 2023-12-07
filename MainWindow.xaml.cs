784945a027e9f14dd717e3ac38443c2ba1493ebd
Hunor József Nagy <Nhunor06@gmail.com>
2023-12-07 10:57:55 +0100

    1.0

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace snookerWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Versenyzők> versenyzok = new List<Versenyzők>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            loadVersenyzok("snooker.txt");
+            comboboxFeltoltes();
+        }
+        public void loadVersenyzok(string eleresiUt)
+        {
+            bool elsosor = false;
+            var betoltottSorok = File.ReadAllLines(eleresiUt);
+            foreach (var item in betoltottSorok)
+            {
+                if (elsosor)
+                {
+                versenyzok.Add(new Versenyzők(item));
+                }
+                else
+                {
+                    elsosor = true;
+                }
+            }
+        }
+
+        private void btnF3_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"3. Feladat: A világranglistán {versenyzok.Count} versenyző szerepel");
+        }
+
+        private void btnF4_Click(object sender, RoutedEventArgs e)
+        {
+            List<double> atlagGBP = new List<double>();
+
+            for (int i = 0; i < versenyzok.Count; i++)
+            {
+                atlagGBP.Add(versenyzok[i].Nyeremeny);
+            }
+            MessageBox.Show($"4. feladat: A versenyzők átlagosan {Math.Round(atlagGBP.Average(),2)} fontot kerestek");
+        }
+        private void comboboxFeltoltes()
+        {
+            List<string> orszgok = new List<string>();
+            foreach (var item in versenyzok)
+            {
+                orszgok.Add(item.Orszag);
+            }
+            orszgok.Sort();
+            foreach (var item in orszgok.Distinct())
+            {
+                cbOrszag.Items.Add(item);
+            }
+        }
+    }
+}
---
+++ Versenyzők.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace snookerWPF
+{
+    class Versenyzők
+    {
+        int helyezes;
+        string nev;
+        string orszag;
+        int nyeremeny;
+
+
+        public Versenyzők(int helyezes,string nev,string orszag,int nyeremeny)
+        {
+            this.helyezes = helyezes;
+            this.nev = nev;
+            this.orszag = orszag;
+            this.nyeremeny = nyeremeny;
+        }
+        public Versenyzők(string sor) 
+        {
+            List<string> elemekLista = sor.Split(';').ToList();
+            this.helyezes = int.Parse(elemekLista[0]);
+            this.nev = elemekLista[1];
+            this.orszag = elemekLista[2];
+            this.nyeremeny = int.Parse(elemekLista[3]);
+        }
+        public int Helyezes { get => helyezes; }
+        public string Nev { get => nev;}
+        public string Orszag { get => orszag;}
+        public int Nyeremeny { get => nyeremeny;}
+    }
+}
